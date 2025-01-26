e35c191d72cd45ec76f508517f2b47e47bda1001
RjayBrown <rjbrown.dev@outlook.com>
2025-01-25 22:09:00 -0500

    added dashboard home cards

--- client/src/pages/DashboardHome.jsx
+++ client/src/pages/DashboardHome.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const DashboardHome = () => {
-	return <h1>Dashboard Home</h1>;
+	return (
+		<>
+			<section className="home card" tabIndex={0}>
+				<h6>Welcome</h6>
+				<p>You have 65 new nessages</p>
+			</section>
+			<section className="home card" tabIndex={0}>
+				<h6>Welcome</h6>
+				<p>You have 65 new nessages</p>
+			</section>
+		</>
+	);
 };
 
 export default DashboardHome;
